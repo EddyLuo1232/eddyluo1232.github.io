49f8db68fee9908da9cf39045e1a693f0e2f8199
Eddy <112454068+EddyLuo1232@users.noreply.github.com>
2026-06-28 11:10:53 -0700

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,22 +2,37 @@ import React, { useRef } from 'react';
 import { useLanguage } from '../../contexts/LanguageContext';
 import './Footer.css';
 
-const VISITOR_MAP_SCRIPT = '//mapmyvisitors.com/map.js?d=gWFCgejqrcPF2cqffm2nGfYs_PdNJ6MVAEIHnllXpqo&cl=ffffff&w=a';
+const VISITOR_MAP_SCRIPTS = [
+  {
+    id: 'mapmyvisitors-original',
+    src: '//mapmyvisitors.com/map.js?d=gWFCgejqrcPF2cqffm2nGfYs_PdNJ6MVAEIHnllXpqo&cl=ffffff&w=a'
+  },
+  {
+    id: 'mapmyvisitors-hidden',
+    src: '//mapmyvisitors.com/map.js?d=7CTeFyC3wT_A3DlE7A976YOUbxvdVPWluGcvGoy_E6A&cl=ffffff&w=a'
+  }
+];
 
 const Footer: React.FC = () => {
   const { t } = useLanguage();
   const mapContainerRef = useRef<HTMLDivElement>(null);
 
   const handleMapTrigger = () => {
-    if (!mapContainerRef.current || document.getElementById('mapmyvisitors')) {
+    if (!mapContainerRef.current) {
       return;
     }
 
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.id = 'mapmyvisitors';
-    script.src = VISITOR_MAP_SCRIPT;
-    mapContainerRef.current.appendChild(script);
+    VISITOR_MAP_SCRIPTS.forEach(({ id, src }) => {
+      if (document.getElementById(id)) {
+        return;
+      }
+
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.id = id;
+      script.src = src;
+      mapContainerRef.current?.appendChild(script);
+    });
   };
 
   return (
